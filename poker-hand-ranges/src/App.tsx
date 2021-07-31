10f14eba163991a3a7b7fb4507a250c47183c58f
Mészáros Martin <martin44548@gmail.com>
2021-07-31 23:27:48 +0200

    change mobx version to older

--- poker-hand-ranges/src/App.tsx
+++ poker-hand-ranges/src/App.tsx
@@ -5,30 +5,30 @@ import { Provider } from "mobx-react";
 import { MainStore } from "./stores/MainStore";
 
 interface IAppProps {
-  MainStore?: MainStore;
+    MainStore?: MainStore;
 }
 
 export default class App extends Component<IAppProps> {
-  private stores = { MainStore: new MainStore() };
+    private stores = { MainStore: new MainStore() };
 
-  // stores = null;
-  // constructor(props) {
-  //   super(props);
+    // stores = null;
+    // constructor(props) {
+    //   super(props);
 
-  //   this.stores = { MainStore: new MainStore() };
-  // }
+    //   this.stores = { MainStore: new MainStore() };
+    // }
 
-  render() {
-    // const stores = { MainStore: new MainStore() };
+    render() {
+        // const stores = { MainStore: new MainStore() };
 
-    return (
-      <div className="App">
-        <Provider {...this.stores}>
-          <HandRangeTemplate />
-        </Provider>
-      </div>
-    );
-  }
+        return (
+            <div className="App">
+                <Provider {...this.stores}>
+                    <HandRangeTemplate />
+                </Provider>
+            </div>
+        );
+    }
 }
 
 // export default App;
--- poker-hand-ranges/src/components/HandRangeTemplate/HandRangeTemplate.tsx
+++ poker-hand-ranges/src/components/HandRangeTemplate/HandRangeTemplate.tsx
@@ -1,4 +1,3 @@
-import { makeObservable } from "mobx";
 import { inject, observer } from "mobx-react";
 import React, { Component } from "react";
 import { MainStore } from "../../stores/MainStore";
@@ -18,22 +17,22 @@ import styles from "./HandRangeTemplate.module.scss";
 // },
 // ];
 
-const PFI_STRATEGY = [
-  {
-    name: "sb",
-    stack: 25,
-    action: "limp",
-    versus: "btn",
-  },
-  {
-    name: "btn",
-    stack: 60,
-    action: "raise",
-    versus: "utg1",
-  },
-];
+// const PFI_STRATEGY = [
+//   {
+//     name: "sb",
+//     stack: 25,
+//     action: "limp",
+//     versus: "btn",
+//   },
+//   {
+//     name: "btn",
+//     stack: 60,
+//     action: "raise",
+//     versus: "utg1",
+//   },
+// ];
 
-const cards = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"];
+// const cards = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"];
 
 // const handRange = [
 //     {
@@ -42,40 +41,40 @@ const cards = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"];
 // ];
 
 interface IHandRangeTemplateProps {
-  MainStore?: MainStore;
+    MainStore?: MainStore;
 }
 
 @inject("MainStore")
 @observer
 export default class HandRangeTemplate extends Component<IHandRangeTemplateProps> {
-  handRange = [];
+    handRange = [];
 
-  constructor(props: any) {
-    super(props);
+    // constructor(props: any) {
+    //     super(props);
+    // }
 
-    makeObservable(this);
-  }
+    componentDidMount() {
+        this.props.MainStore!.createRange();
+    }
 
-  render() {
-    this.props.MainStore!.createRange();
-
-    return (
-      <div>
-        {this.props.MainStore!.handRange.map((row) => (
-          <div className={styles.row}>
-            {row.map((cell) => (
-              <div
-                // onClick={() => this.changeHandRange(cell.card)}
-                className={`${styles.cardCell} ${
-                  cell.clicked ? styles.clicked : null
-                }`}
-              >
-                {cell.card}
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                {this.props.MainStore!.handRange.map((row) => (
+                    <div className={styles.row}>
+                        {row.map((cell) => (
+                            <div
+                                // onClick={() => this.changeHandRange(cell.card)}
+                                className={`${styles.cardCell} ${
+                                    cell.clicked ? styles.clicked : null
+                                }`}
+                            >
+                                {cell.card}
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        );
+    }
 }
